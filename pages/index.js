ff6b3df88f238b9666db72bf0743756eaabe36f4
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-01-20 20:15:04 -0300

    Modify styles the section links on the page home

--- pages/index.js
+++ pages/index.js
@@ -17,15 +17,15 @@ export default function Home() {
           </div>
           <Logo widthImg={500} heightImg={500} />
         </div>
-        <div className="grid grid-cols-2 gap-1 place-items-center">
-          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-40 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000">
-            <Link href="#services">Ver servicios</Link>
+        <div className="grid grid-cols-3 gap-4 place-items-center">
+          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-28 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000">
+            <Link href="#services">Servicios</Link>
           </span>
-          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-40 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000">
-            <Link href="#blog">Ver posteos</Link>
+          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-28 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000">
+            <Link href="#blog">Blog</Link>
           </span>
-          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-40 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000 col-span-2">
-            <Link href="#projects">Ver trabajos</Link>
+          <span className="text-center mt-10 text-base border-solid border-2 border-b-4 rounded-full dark:border-gray-200 w-28 dark:hover:border-yellow-200 text-gray-700 border-gray-700 hover:bg-gray-700 hover:text-gray-200 dark:text-gray-200 dark:hover:text-yellow-200 antialiased transition-colors duration-1000">
+            <Link href="#projects">Proyectos</Link>
           </span>
         </div>
         <div className="mt-20">
