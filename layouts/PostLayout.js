6b24c91328b7c4189a431dd68d8a2e91a91ca7f1
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-01-20 22:22:10 -0300

    Comments shared links for bug detected

--- layouts/PostLayout.js
+++ layouts/PostLayout.js
@@ -100,12 +100,12 @@ export default function PostLayout({ frontMatter, authorDetails, next, prev, chi
                 </ul>
               </dd>
             </dl>
-            <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 grid grid-flow-col">
+            {/* <div className="divide-y divide-gray-200 pb-8 dark:divide-gray-700 xl:divide-y-0 grid grid-flow-col">
               <h3>Compártelo en las redes</h3>
               <SocialIcon kind="facebook" href={sharedRoutes.facebook} size="6" />
               <SocialIcon kind="twitter" href={sharedRoutes.twitter} size="6" />
               <SocialIcon kind="linkedin" href={sharedRoutes.linkedin} size="6" />
-            </div>
+            </div> */}
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pt-10 pb-8 dark:prose-dark">{children}</div>
             </div>
