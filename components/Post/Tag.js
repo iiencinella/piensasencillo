62657e53b53ae4ee902162e43cd8ff2b2eeddb56
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-02-06 12:48:12 -0300

    Modify style of underline

--- components/Post/Tag.js
+++ components/Post/Tag.js
@@ -4,7 +4,7 @@ import kebabCase from '@/lib/utils/kebabCase'
 const Tag = ({ text }) => {
   return (
     <Link href={`/tags/${kebabCase(text)}`}>
-      <a className="mr-3 text-sm font-medium uppercase text-gray-900 hover:underline hover:underline-offset-1 dark:text-yellow-200">
+      <a className="mr-3 text-sm font-medium uppercase text-gray-900 hover-underline-animation dark:text-yellow-200">
         {text.split(' ').join('-')}
       </a>
     </Link>
