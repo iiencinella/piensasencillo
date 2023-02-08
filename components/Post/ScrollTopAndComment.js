9bfb84765fe8b9d572215fd1245a2e3cab193081
Iván Ezequiel Iencinella <iiencinella@outlook.es>
2023-02-08 14:20:54 -0300

    Add efect bounce at the scroll up

--- components/Post/ScrollTopAndComment.js
+++ components/Post/ScrollTopAndComment.js
@@ -44,7 +44,7 @@ const ScrollTopAndComment = () => {
         onClick={handleScrollTop}
         className="rounded-full bg-gray-900 p-2 text-white transition-all hover:bg-gray-300 dark:bg-gray-200 dark:text-gray-700 dark:hover:bg-gray-600"
       >
-        <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+        <svg className="h-5 w-5 animate-bounce" viewBox="0 0 20 20" fill="currentColor">
           <path
             fillRule="evenodd"
             d="M3.293 9.707a1 1 0 010-1.414l6-6a1 1 0 011.414 0l6 6a1 1 0 01-1.414 1.414L11 5.414V17a1 1 0 11-2 0V5.414L4.707 9.707a1 1 0 01-1.414 0z"
